602766950d12f4a75273ec55f79919fc8da4eb61
Geoson Viray <geovir.26@gmail.com>
2025-01-06 23:47:37 +0800

    My 2d portfolio

---
+++ src/constants.js
@@ -0,0 +1,23 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my PC. I work mostly in JavaScript/PHP these days.
+    I also like React and Python. Anyway regardless of the language, I just like programming.
+    Here is my <a href="https://github.com/vinxl26" target="_blank">Github</a>!`,
+  "cs-degree": `This are my badges. I hung it on the wall come and see it <a href=https://www.credly.com/badges/20827381-748a-4a3e-896f-eef21efdefae target="_blank">ISC2</a>,<a href=https://www.credly.com/badges/bbc72498-e2cd-456d-b6b1-23f47d9b8f93 target="_blank">IBM</a>.`,
+  "sofa-table": `That's my sofa. I like to relax here and watch YouTube. 
+  I also watch make game programming tutorials on YouTube. Go subscribe to <a href="https://youtube.com/@jslegenddev" target="_blank">JSLEGENDDEV</a>! (If you like the content)
+  You'll learn how I built this portfolio you're currently playing through!`,
+  tv: `That's my TV. I've been watching tech youtubers a lot recently like :
+   <a href="https://www.youtube.com/@jslegendev" target="_blank">JSLegendDev</a>, <a href="https://www.youtube.com/@javascriptmastery" target="_blank">JS Mastery</a>,
+  <a href="https://www.youtube.com/@NetworkChuck" target="_blank">NetworkChuck</a> (sometimes) and <a href="https://www.youtube.com/@Codevolution" target="_blank">Codevolution</a>!`,
+  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I had to write it down and do it on my pc.`,
+  resume: `This is my desk and on it is my resume. <a href="https://geovir.netlify.app" target="_blank">Check it out?</a>
+  Contact me at geovir.26@gmail.com if you have any interesting job opportunities!`,
+  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about how to use
+  Kaboom.js by watching some of the tutorials by JSlegenddev <a href="https://youtube.com/@jslegenddev" target="_blank">here</a>.`,
+  library: `There are a lot of programming books on my shelves. Checking the newest learning materials hmm....
+  I probably only read one of them. Who else compulsively buys technical books without ever finishing them?`,
+  exit: `If you want to exit Geo's portfolio, just close the tab.`,
+};
